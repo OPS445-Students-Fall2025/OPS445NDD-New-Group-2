9841e27a980aa08a621f4bd5dd9a311051769384
Enrique Molina <emolina3@myseneca.ca>
2025-12-02 21:28:52 +0000

    First Version of file (v1.0)

---
+++ emSambareport.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3 
+'''
+Enrique Molina 164024226
+'''
+import os 
+'''
+This Script is to audit the Samba log files to see who is currently accessing the samba filesystem, on what operating system, what time, the IP address, which file accessed. 
+'''
+
+
